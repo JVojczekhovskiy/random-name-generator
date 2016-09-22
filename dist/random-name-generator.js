40b2f9d945281c09001d5198ec63dd63076ae883
JVojczekhovskiy <j.vojczekhovskiy@mondriaanict.nl>
2016-09-22 13:50:04 +0200

    added checknickname function.

--- dist/random-name-generator.js
+++ dist/random-name-generator.js
@@ -36,7 +36,7 @@ angular.module('random-name-generator',[])
     deferred.resolve($http.get(this.path+'?nocache'+new Date().getTime()).then(function(response){
       for (var i = 0; i < response.data.firstNames.male.length; i++) if (response.data.firstNames.male[i] == firstName) valid[0] = true;
       for (var i = 0; i < response.data.lastNames.length; i++) if (esponse.data.lastNames[i] == lastName) valid[1] = true;
-      if (valid[0]&&valid[1]) return true else return false;
+      (valid[0]&&valid[1])? return true : return false;
     }));
     return deferred.promise;
   }
