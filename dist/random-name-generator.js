8f2c8fc97843248f59497d4c8d305d10b51a7147
JVojczekhovskiy <j.vojczekhovskiy@mondriaanict.nl>
2016-10-20 16:16:19 +0200

    new no chache

--- dist/random-name-generator.js
+++ dist/random-name-generator.js
@@ -4,7 +4,7 @@ angular.module('random-name-generator',[])
   var self = this;
   self.initialize = function(p){
     self.path = p;
-    $http({method: 'GET', url: self.path, params: { 'nocache': new Date().getTime() }}).then(function(response){
+    $http.get(self.path, { headers: { 'Cache-Control' : 'no-cache' } }).then(function(response){
       self.names = response.data;
     });
   }
@@ -12,7 +12,7 @@ angular.module('random-name-generator',[])
   self.generateName = function(in1,in2,gender){
     var deferred = $q.defer();
     if(self.path!=undefined){
-      deferred.resolve($http({method: 'GET', url: self.path, params: { 'nocache': new Date().getTime() }}).then(function(response){
+      deferred.resolve($http.get(self.path, { headers: { 'Cache-Control' : 'no-cache' } })).then(function(response){
         self.names = response.data
         var temp1 =[],temp2 =[];
         if(gender=="male"){
